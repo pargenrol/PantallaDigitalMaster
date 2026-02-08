81256d10e3ab00014d1e82425aeb2270fdb00ef6
JLToledano <jl.toledano.2018@alumnos.urjc.es>
2026-02-08 22:35:10 +0100

    Create instance folder if it does not exist

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
 
 class Config:
     SECRET_KEY = 'rpg-master-secret'
+    os.makedirs(INSTANCE_DIR, exist_ok=True)
+
     SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "app.db")
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "app.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -18,8 +20,8 @@ class Config:
     SPELLS_DIR = os.path.join(BASE_DIR, "resources", "spells")
     RULES_DIR = os.path.join(BASE_DIR, "resources", "rules")
 
-    WHITEBOARD_STATE_FILE = os.path.join(BASE_DIR, "instance", "whiteboard_state.json")
-    SCREEN_COMMAND_FILE = os.path.join(BASE_DIR, "instance", "screen_command.json")
+    WHITEBOARD_STATE_FILE = os.path.join(INSTANCE_DIR, "whiteboard_state.json")
+    SCREEN_COMMAND_FILE = os.path.join(INSTANCE_DIR, "screen_command.json")
 
 
 class DevelopmentConfig(Config):
