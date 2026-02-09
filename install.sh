b6b6c6934a68a38ea3ac1946818705f71057290c
JLToledano <jl.toledano.2018@alumnos.urjc.es>
2026-02-09 17:18:17 +0100

    Fix instalation and run of app in Linux

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+set -e
 
 echo "--- 🐉 Instalando Centro de Mando RPG ---"
 
-# Comprobar si python3 está instalado
-if ! command -v python3 &> /dev/null
-then
+# Check if python3 is installed
+if ! command -v python3 >/dev/null 2>&1; then
     echo "❌ Error: Python 3 no encontrado. Por favor instálalo antes de continuar."
     exit 1
 fi
 
-echo "1. Creando entorno virtual (venv)..."
-python3 -m venv venv
+# Create env if it does not exist or reuse it if it does
+if [ ! -d "venv" ]; then
+    echo "1. Creando entorno virtual (venv)..."
+    python3 -m venv venv
+else
+    echo "1. El entorno virtual ya existe. Se reutiliza."
+fi
 
 echo "2. Activando entorno..."
 source venv/bin/activate
 
 echo "3. Actualizando pip e instalando librerías..."
-pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install --upgrade pip setuptools wheel
+python -m pip install -r requirements.txt
 
 echo ""
 echo "--- ✅ Instalación completada ---"
